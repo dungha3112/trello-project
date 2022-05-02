22544bf8d3932823adf0bac84b5b76d9ce8bc0ae
ktdung113 <ktdung113@gmail.com>
2022-05-02 14:00:16 +0700

    clear_architecture.

---
+++ front-end/src/components/BoardContent/BoardContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BoardContent.scss';
+import Column from 'components/Column/Column';
+
+const BoardContent = () => {
+  return (
+    <>
+      <div className="board-contents">
+        <Column />
+
+      </div>
+    </>
+  )
+}
+
+export default BoardContent;
