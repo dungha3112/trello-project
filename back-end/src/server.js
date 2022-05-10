ea4b88029ad9619d555238bc1ace2b2903913e8f
ktdung113 <ktdung113@gmail.com>
2022-05-10 16:31:47 +0700

    Config babeljs .

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import {mapOrder} from './utilities/sorts.js'
+/* eslint-disable no-console */
+import { mapOrder } from '@/utilities/sorts.js'
 
-const app = express();
+const app = express()
 const hostname = 'localhost'
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.end('<h1>Welcome</h1>')
+  res.end('<h1>Hello word!c</h1><br/>')
 })
 
 app.listen(port, hostname, () => {
